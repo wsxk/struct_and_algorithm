2fa9d48b1c807d9816d50be25473e22c3ed01740
wsxk <79676829+wsxk@users.noreply.github.com>
2021-09-14 10:46:24 +0800

    Add files via upload

---
+++ leetcode刷题/二叉搜索树和双向链表.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        left = NULL;
+        right = NULL;
+    }
+
+    Node(int _val, Node* _left, Node* _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+class Solution {
+private:
+    Node *pre, *head;
+    void dfs(Node* cur) {
+        if(cur == nullptr) return;
+        dfs(cur->left);
+        if(pre != nullptr) pre->right = cur;
+        else head = cur;
+        cur->left = pre;
+        pre = cur;
+        dfs(cur->right);
+    }
+public:
+    Node* treeToDoublyList(Node* root) {
+        if(root==nullptr)
+            return root;
+        dfs(root);
+        head->left=pre;
+        pre->right=head;
+        return head;
+    }
+};
